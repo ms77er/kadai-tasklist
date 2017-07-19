4dca2029d4431e66217d41d9d3ebb425c4352a6f
M.Seto <ms77er@gmail.com>
2017-07-19 12:17:34 +0000

    lesson12

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -67,5 +69,13 @@ class TasksController < ApplicationController
   def set_task
     @task = Task.find(params[:id])
   end
+  
+    def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
   
 end
